d07c885ec9dac9a5048938b8c9bbae95bddd28c4
Odin Daza <187565420+od1nd@users.noreply.github.com>
2025-02-09 19:25:38 -0500

    Feature HEAP Algorithm: Implementing the heap algorithm to generate the permutation of the assigned words

--- cdg.py
+++ cdg.py
@@ -1,5 +1,28 @@
 import argparse
 
+def generate(size_list_words: int, list_words: list[str]) -> None:
+    temp_list: list[int] = []
+
+    for index in range(0, size_list_words):
+        temp_list.append(0)
+
+    print("".join(word for word in list_words))
+
+    index: int = 0
+    while index < size_list_words:
+        if temp_list[index] < index:
+            if index % 2 == 0:
+                list_words[0], list_words[index] = list_words[index], list_words[0]
+            else:
+                list_words[temp_list[index]], list_words[index] = list_words[index], list_words[temp_list[index]]
+
+            print("".join(word for word in list_words))
+            temp_list[index] += 1
+            index = 0
+        else:
+            temp_list[index] = 0
+            index += 1        
+
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("-a", "--add", dest="add", action="extend", nargs='+', help="Add words to the dictionary")
@@ -8,8 +31,7 @@ def main() -> None:
     args = parser.parse_args()
 
     if(args.add and args.output):
-        print("Words: " + str(args.add))
-        print("Output: " + args.output)
+        generate(len(args.add), args.add)
     else:
         parser.print_help()
 
